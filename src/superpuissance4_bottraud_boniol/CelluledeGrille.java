249e8163b4ad86e5afd901c3b50a601467103735
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-11-28 18:53:21 +0100

    création des fonctions

--- src/superpuissance4_bottraud_boniol/CelluledeGrille.java
+++ src/superpuissance4_bottraud_boniol/CelluledeGrille.java
@@ -52,14 +52,61 @@ public class CelluledeGrille {
             return false;
         }
     }
-    public boolean placerDésintegrateur(){
-        if (désintegrateur==false){
-            désintegrateur= true;
+
+    public boolean placerDésintegrateur() {
+        if (désintegrateur == false) {
+            désintegrateur = true;
             System.out.println("Ajout d'un désintegrateur");
             return true;
-        } else{
+        } else {
             System.out.println("désintegrateur déjà présent");
             return false;
         }
     }
+
+    public boolean présenceTrouNoir() {
+        if (trouNoir == true) {
+            System.out.println("trou noir présent");
+            return true;
+        } else {
+            System.out.println("pas de trou noir");
+            return false;
+        }
+    }
+
+    public boolean présenceDésintegrateur() {
+        if (désintegrateur == true) {
+            System.out.println("trou noir présent");
+            return true;
+        } else {
+            System.out.println("pas de désintegrateur");
+            return false;
+        }
+    }
+
+    public String lireCouleurJeton() {
+        if (JetonGrille == null) {
+            return "cellule vide";
+        } else {
+            return JetonGrille.LireCouleur();
+        }
+    }
+
+    public boolean recupererDésintégrateur() {
+        if (désintegrateur == true){
+        désintegrateur = false;
+        System.out.println("Suppresion du désintegrateur");
+        return true;
+        } else{
+            System.out.println("pas de désintegrateur");
+            return false;
+        }
+
+    
+    
+
+
+        
+    
 }
+    }
