2913790e612bcb9794fe6597ce51c51072108931
Benjamin Boniol <benjamin.boniol@epfedu.fr>
2022-11-30 15:23:32 +0100

    Création de la classe joueur

---
+++ src/superpuissance4_bottraud_boniol/joueur.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superpuissance4_bottraud_boniol;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class joueur {
+    String nom;
+    String couleur;
+    Jeton reserveJetons = new Jeton[21];
+    int nombreDesintegrateurs;
+
+}
+
