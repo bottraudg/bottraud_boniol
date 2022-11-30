388d723764269e4890aeed0c293a7b8fc1865256
guilenebottraud <guilenebottraud@10.3.61.219>
2022-11-30 15:30:22 +0100

    rajout de viderGrille, GrilleSurConsole

--- src/superpuissance4_bottraud_boniol/PlateauDeJeu.java
+++ src/superpuissance4_bottraud_boniol/PlateauDeJeu.java
@@ -18,16 +18,16 @@ public class PlateauDeJeu {
      }   
    }
  }    
-public boolean ajouterJetonDansColonne(Jeton, int){ // prend en parametre un jeton et une colonne 
+public boolean ajouterJetonDansColonne(Jeton unJeton, int c){ // prend en parametre un jeton et une colonne 
     for (int l=0; l<6; l++){ // parcours les lignes 
         if (grille[l][c].jetonCourant == null){ // on affecte le jeton dès qu'une case sur la colonne est vide 
-        grille[l][c].affecterJeton(uneJeton);
+        grille[l][c].affecterJeton(unJeton);
         return true;
         }
     }
 return false; // colonne remplie 
 }
-public boolean grilleRemplie()
+public boolean grilleRemplie(){
        for (int l=0; l<6; l++){ //parcours le tableau grille
         for (int c=0;c<7; c++){
             if (grille[l][c] != null){ // si toutes les cases sont remplies
@@ -36,4 +36,40 @@ public boolean grilleRemplie()
         }
         }
 }
+public void viderGrille(Joueur, Joueur){ //initialise les données a 0 pour toutes les cellules, les trous noirs et les desintegrateurs 
+       for (int l=0; l<6; l++){ 
+        for (int c=0;c<7; c++){  
+            grille[l][c].jetonCourant = null;
+            grille[l][c].TrouNoir = false;
+            grille [l][c].desintegrateur= false;
+            
+                    
+        }
+}
+public void GrilleSurConsole() { // afficher le grille dans la console 
+    for(int l=5; l=0; l>=0); l--){ // bloucle décrémentée car l'affichage conventionnel et celui dans les tableaux sont inversé 
+    for (int c=0; c<7; c++){
+        if (grille[l][c].TrouNoir != false{
+            System.out.print("T"); // T sur la cellule pour signifier sa presence
+        }
+        else if(grille [l][c].desintegrateur !=false){
+            System.out.print("D");
+        }
+        else if (grille[l][c].jetonCourant == null){
+            System.out.print("N");
+        }
+        else if ((grille[l][c].jetonCourant.Couleur)!= "Rouge"){
+            System.out.print("J");
+        }
+        else{ System.out.print("R");
+        }
+    }
+    System.out.println(""+ (l+1)); // affichage des numéros de lignes (l+1)car le tableau est initié a 0
+}
+    for (int c=0; c<7; c++){
+        System.out.print((c+1)); //affichage des colonnes (c+1) car tableau commence à 0
+    }
+    System.out.println();// affichage global du tableau    
+}
+
 }
