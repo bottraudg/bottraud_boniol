85ea149f083cee1ba9adb4f031de3972c37da658
guilenebottraud <guilenebottraud@air-de-guilene>
2022-12-28 16:38:20 +0100

    creation de la classe partie, paufinement de la classe joueur

---
+++ src/superpuissance4_bottraud_boniol/Partie.java
@@ -0,0 +1,20 @@
+package superpuissance4_bottraud_boniol;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author guilenebottraud
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public final class Partie {
+
+    joueur[] ListeJoueurs = new joueur[2];
+    joueur JoueurCourant;
+    Grille grilleJeu = new Grille();
+
+   
